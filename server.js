2bf7c99778b5ff4fbd72480e30bb6fd7f12959cf
Filip <filip.knias@o2.pl>
2020-07-12 21:03:17 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+
+// Server config
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+// MongoDB config
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to DB"));
+
+// Routes imports
+const indexRouter = require("./routes/index");
+
+// Routes
+app.get("/", indexRouter);
+
+app.listen(process.env.PORT || 3000);
