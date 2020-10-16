3974f13e4ffb6a5c7a48887348ce82558973b5dd
Filip <filip.knias@o2.pl>
2020-10-16 17:24:04 +0200

    Profile page v3 (overview component done)

--- client/src/components/profile/Overview.js
+++ client/src/components/profile/Overview.js
@@ -1,48 +1,145 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 // Bootstrap
 import Image from "react-bootstrap/Image";
-import Button from "react-bootstrap/Button";
+import Spinner from "react-bootstrap/Spinner";
+// Components
+import Error from '../Error';
 // Images
 import ProfileAvatar from '../img/profile-avatar.svg';
-// Context
-import { UserContext } from '../../context/UserContext';
-// Reducer types
-import { CLEAR_USER } from '../../reducers/types';
+import SurveysStatsImage from '../img/surveys-stats-image.svg';
+import ActiveSurveysIcon from '../img/active-surveys-icon.svg';
+import ClosedSurveysIcon from '../img/closed-surveys-icon.svg';
 
 export default function Overview({ userId }) {
-    // Context
-    const [userState, dispatch] = useContext(UserContext);
+    // State
+    const [user, setUser] = useState({});
+    const [surveys, setSurveys] = useState([]);
+    const [activeSurveysCount, setActiveSurveysCount] = useState(null);
+    const [closedSurveysCount, setClosedSurveysCount] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-    // Log out user
-    const handleLogout = () => {
-    dispatch({ type: CLEAR_USER });
-    localStorage.removeItem("auth-token");
-    };
+    // Get surveys by given user
+    useEffect(() => {
+        // Start loading
+        setLoading(true);
+        axios.get(`/api/users/${userId}`)
+            .then(res => {
+                // Set user
+                setUser(res.data);
+                // Stop loading
+                setLoading(false);
+            })
+            .catch(err => {
+                // Set error
+                setError(err.response.data.error);
+                // Stop loading
+                setLoading(false);
+            })
+
+        axios.get(`/api/surveys/${userId}`)
+            .then(res => {
+                // Set surveys
+                setSurveys(res.data);
+                // Stop loading
+                setLoading(false);
+            })
+            .catch(err => {
+                // Set error
+                setError(err.response.data.error);
+                // Stop loading
+                setLoading(false);
+            })
+    }, []);
+
+    // Count surveys based on their status
+    useEffect(() => {
+        // Set active surveys count
+        const activeSurveys = surveys.filter((survey) => {
+            return survey.status === "public";
+        });
+        setActiveSurveysCount(activeSurveys.length);
+
+        // Set closed surveys count
+        const closedSurveys = surveys.filter((survey) => {
+            return survey.status === "closed";
+        });
+        setClosedSurveysCount(closedSurveys.length);
+    }, [surveys]);
 
     return (
         <>  
-            <div className="d-flex flex-column flex-md-row align-items-center align-items-md-start text-center text-md-left">
-                <Image src={ProfileAvatar} height="140" />
-                <div className="mt-4 mt-md-0 ml-md-3">
-                    <div className="mb-3">
-                        <span className="profile-label">Display name</span>
-                        <h3>{userState.user.displayName}</h3>
-                    </div>
-                    <div>
-                        <span className="profile-label">E-mail</span>
-                        <h3>{userState.user.email}</h3>
-                    </div>
-                </div>
-            </div>
-            {userState.user._id === userId && (
-                <Button 
-                    variant="primary" 
-                    onClick={handleLogout}
-                    className="mt-5 mb-3"
-                    size="lg"
-                >
-                    Log Out
-                </Button>
+            {error ? (
+                <Error message={error} />
+            ) : (
+                <>
+                    {loading ? (
+                        <Spinner animation="border" className="mx-auto d-block" />
+                    ) : (
+                        <>
+                            <section>
+                                <h3 className="mb-4 text-center text-md-left">Profile</h3>
+                                <div className="
+                                    d-flex 
+                                    flex-column 
+                                    flex-md-row 
+                                    align-items-center 
+                                    align-items-md-start 
+                                    text-center 
+                                    text-md-left"
+                                >
+                                    <Image src={ProfileAvatar} height="140" />
+                                    <div className="mt-4 mt-md-0 ml-md-4">
+                                        <div className="mb-3">
+                                            <span className="profile-label">Display name</span>
+                                            <h3>{user.displayName}</h3>
+                                        </div>
+                                        <div>
+                                            <span className="profile-label">E-mail</span>
+                                            <h3>{user.email}</h3>
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+
+                            <section className="mt-5 mb-3">
+                                <h3 className="mb-4 text-center text-md-left">Surveys Statistics</h3>
+                                <div className="
+                                    d-flex 
+                                    flex-column 
+                                    flex-md-row 
+                                    align-items-center 
+                                    align-items-md-start 
+                                    text-center 
+                                    text-md-left 
+                                    mt-4"
+                                >
+                                    <Image src={SurveysStatsImage} height="140" />
+                                    <div className="mx-4 mt-4 mt-md-0">
+                                        <p className="profile-label text-center">Created surveys</p>
+                                        <div className="d-flex mt-2">
+                                            <div className="mr-4">
+                                                <div className="d-flex">
+                                                    <Image src={ActiveSurveysIcon} height="30" className="mr-2" />
+                                                    <h4>{activeSurveysCount}</h4>
+                                                </div>
+                                                <p className="profile-label text-center mt-1">Active</p>
+                                            </div>
+                                            <div>
+                                                <div className="d-flex">
+                                                    <Image src={ClosedSurveysIcon} height="30" className="mr-2" />
+                                                    <h4>{closedSurveysCount}</h4>
+                                                </div>
+                                                <p className="profile-label text-center mt-1">Closed</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+                        </>
+                    )}
+                </>
             )}
         </>
     )
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,52 +1,35 @@
-import React, { useContext } from "react";
+import React from "react";
 // Bootstrap
 import Card from 'react-bootstrap/Card';
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
 // Components
-import AuthError from "../components/AuthError";
 import Overview from "../components/profile/Overview";
-// Context
-import { UserContext } from '../context/UserContext';
 
 export default function Profile(props) {
-    // Context
-    const [userState, dispatch] = useContext(UserContext);
-
     const userId = props.match.params.userId;
 
     return (
-        <>
-            {!userState.isAuth ? (
-                <AuthError />
-            ) : (
-                <Card border="dark">
-                    <Card.Header className="text-center" as="h4">
-                        Profile <span className="green-text">Details</span>
-                    </Card.Header>
-                    <Card.Body className="px-md-5">
-                        <Tabs 
-                            defaultActiveKey="overview" 
-                            id="profile-tabs" 
-                            className="justify-content-md-center justify-content-between mb-5"
-                        >
-                            <Tab eventKey="overview" title="Overview">
-                                <Overview userId={userId} />
-                            </Tab>
-                            <Tab eventKey="surveys" title="Surveys">
-                                <h4>
-                                    Surveys
-                                </h4>
-                            </Tab>
-                            <Tab eventKey="edit" title="Edit Profile">
-                                <h4>
-                                    Edit Profile
-                                </h4>
-                            </Tab>
-                        </Tabs>  
-                    </Card.Body>
-                </Card>
-            )}
-        </>
+        <Card border="dark">
+            <Card.Header className="text-center" as="h4">
+                Profile <span className="green-text">Details</span>
+            </Card.Header>
+            <Card.Body className="px-md-5">
+                <Tabs 
+                    defaultActiveKey="overview" 
+                    id="profile-tabs" 
+                    className="mb-5"
+                >
+                    <Tab eventKey="overview" title="Overview">
+                        <Overview userId={userId} />
+                    </Tab>
+                    <Tab eventKey="surveys" title="Surveys">
+                        <h4>
+                            Surveys
+                        </h4>
+                    </Tab>
+                </Tabs>  
+            </Card.Body>
+        </Card>
     )
 }
